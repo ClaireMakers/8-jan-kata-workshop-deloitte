4f42645ab64d85e22963ef21b673611f4a64a57e
Claire Castanet <clairecastanet@Mac.lan>
2026-01-08 11:52:55 +0000

    updated challenge

--- is_n_divisible_by_numbers.py
+++ is_n_divisible_by_numbers.py
@@ -1,7 +1,17 @@
-# Create a function that checks if the first argument n is divisible by the second argument x and returns true if it is, false if not
+# Level 1: Create a function that checks if the first argument n is divisible by the second argument x and returns true if it is, false if not
 
 # Example:
 # is_divisible(6,3) -> true
 # is_divisible(12,7) -> false
 
 def is_divisible(n,x): 
+
+
+
+# Level 2: Create a function that checks if the first argument n is divisible by all the numbers in the second argument list and returns true if it is, false if not
+
+# Example:
+# is_divisible(6,[2,3]]) -> true
+# is_divisible(12,[7]) -> false
+
+def is_divisible(n, list_of_numbers):
